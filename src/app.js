657bb602babc7e89d72ec886fdab3e8d1f1e56a6
jatingarg850 <jatingarg850@gmail.com>
2026-02-08 01:03:49 +0530

    added

--- src/app.js
+++ src/app.js
@@ -75,9 +75,6 @@ app.use(
   })
 );
 
-// ✅ Handle preflight requests
-app.options("*", cors());
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
